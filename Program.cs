78543c0a4bad8fc0e2b457c6edfa94b4c5a24101
ThOfficialCoder <sehzademar@outlook.com>
2025-02-08 14:07:25 -0500

    Removed Class1 and add some functionality. Rectangles move and ball moves and bounces.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 using Raylib_cs;
 
 public class Program {
@@ -11,6 +12,10 @@ public class Program {
         double rec2Posy = 12;
         Rectangle rec = new Rectangle(recPosx, (float)recPosy, 20, 60);
         Rectangle rec2 = new Rectangle(760, (float)rec2Posy, 20, 60);
+        Vector2 ballPos = new Vector2(400, 300);
+        Vector2 ballVel = new Vector2(0.04f, 0.04f);
+        float ballRadius = 10;
+
 
         while (!Raylib.WindowShouldClose()) {
             Raylib.BeginDrawing();
@@ -19,8 +24,11 @@ public class Program {
             Raylib.DrawRectangleRec(rec, Color.Black);
             Raylib.DrawRectangleRec(rec2, Color.Black);
 
+            ballPos.X += ballVel.X;
+            ballPos.Y += ballVel.Y;
+
             if (Raylib.IsKeyDown(KeyboardKey.S)) {
-               recPosy += 0.1;
+                recPosy += 0.1;
             } else if (Raylib.IsKeyDown(KeyboardKey.W)) {
                 recPosy -= 0.1;
             }
@@ -45,8 +53,41 @@ public class Program {
                 rec2Posy = Raylib.GetScreenHeight() - 60;
             }
 
+            if (ballPos.X - ballRadius < 0 || ballPos.X + ballRadius > Raylib.GetScreenWidth()) {
+                ballVel.X *= -1;
+            }
+
+            if (ballPos.Y - ballRadius < 0 || ballPos.Y + ballRadius > Raylib.GetScreenHeight()) {
+                ballVel.Y *= -1;
+            }
+
+            if (Raylib.CheckCollisionCircleRec(ballPos, ballRadius, rec)) {
+                if (ballVel.Y > 0) {
+                    ballPos.Y = rec.Y - ballRadius;
+                } else if (ballVel.Y < 0) {
+                    ballPos.Y = rec.Y + rec.Height + ballRadius; 
+                }
+                ballVel.X *= -1;
+            }
+
+            if (Raylib.CheckCollisionCircleRec(ballPos, ballRadius, rec2)) {
+                if (ballVel.Y > 0) {
+                    ballPos.Y = rec2.Y - ballRadius;
+                }
+                else if (ballVel.Y < 0) {
+                    ballPos.Y = rec2.Y + rec2.Height + ballRadius;
+                }
+                ballVel.X *= -1;
+            }
+
+
+
             rec2.Y = (float)rec2Posy;
             rec.Y = (float)recPosy;
+
+            
+
+            Raylib.DrawCircleV(ballPos, ballRadius, Color.Black);
 
             Raylib.EndDrawing();
         }
